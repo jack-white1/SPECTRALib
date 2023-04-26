4b85483f025c82734fb1ae0e02aaaf2dd4e2fe0b
Jack White <49734750+jack-white1@users.noreply.github.com>
2023-04-26 20:51:51 +0100

    preparing for python release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spectralib",
-    version="0.0.6",
+    version="0.0.8",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     classifiers=[
